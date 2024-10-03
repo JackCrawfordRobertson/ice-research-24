025be76cd04e72e4e069c98d6b328027967a550f
Jack Robetson <jackrobertsonps3@gmail.com>
2024-10-03 16:47:22 +0100

    Carosel Updates

--- src/app/components/Carousel/Carousel.js
+++ src/app/components/Carousel/Carousel.js
@@ -1,16 +1,15 @@
 "use client";
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css'; // Import Swiper styles
+import { useRef, useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 import dynamic from "next/dynamic";
-import Slider from "react-slick";
-import { useRef, useEffect } from "react";
-import { Suspense } from "react";
-import { useInView } from "react-intersection-observer"; // Import useInView hook
 import styles from "./Carousel.module.css";
-import ThreeColumnGrid from "../Carousel/Summery/ThreeColumnGrid";
-import LoadingSpinner from "../Carousel/LoadingSpinner"; // Loading spinner
 
-// Dynamically import each chart component to lazy load them
+// Dynamically import the slides
 const WelcomeSlide = dynamic(() => import("./WelcomeSlide/WelcomeSlide"), { ssr: false });
+const ThreeColumnGrid = dynamic(() => import("../Carousel/Summery/ThreeColumnGrid"), { ssr: false });
 const ThreeColumnSlide = dynamic(() => import("./ThreeColumnSlide"), { ssr: false });
 const TreemapChart = dynamic(() => import("../Charts/Q1TreeMap"), { ssr: false });
 const Q2StackedBar = dynamic(() => import("../Charts/Q2StackedBar"), { ssr: false });
@@ -23,154 +22,154 @@ const Q8ROI = dynamic(() => import("../Charts/Q8ROI"), { ssr: false });
 const Q9Metrics = dynamic(() => import("../Charts/Q9Metrics"), { ssr: false });
 
 export default function Carousel({ currentSlide, setCurrentSlide }) {
-  const sliderRef = useRef(null); // Create a ref for the slider
+  const [swiperInstance, setSwiperInstance] = useState(null); // Store the Swiper instance
 
+  // Swiper settings
   const settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    swipe: false,
-    beforeChange: (current, next) => {
-      setCurrentSlide(next);
-    },
+    slidesPerView: 1,
+    spaceBetween: 30,
+    onSlideChange: (swiper) => setCurrentSlide(swiper.activeIndex),
+    onSwiper: setSwiperInstance, // Set the Swiper instance on initialization
   };
 
-  // Effect to change slide when currentSlide updates
+  // Programmatically change the slide when currentSlide changes
   useEffect(() => {
-    if (sliderRef.current) {
-      sliderRef.current.slickGoTo(currentSlide); // Programmatically go to the current slide
+    if (swiperInstance && typeof swiperInstance.slideTo === 'function') {
+      swiperInstance.slideTo(currentSlide);
     }
-  }, [currentSlide]);
+  }, [currentSlide, swiperInstance]);
 
   return (
     <div className={styles.sliderWrapper}>
-      <Slider ref={sliderRef} {...settings} className={styles.carousel}>
-        {/* First Slide: WelcomeSlide Component */}
-        <SlideWithInView>
-          <Suspense fallback={<LoadingSpinner />}>
+      <Swiper {...settings}>
+        {/* First Slide */}
+        <SwiperSlide>
+          <InViewSlide>
             <WelcomeSlide setCurrentSlide={setCurrentSlide} />
-          </Suspense>
-        </SlideWithInView>
+          </InViewSlide>
+        </SwiperSlide>
 
-        {/* Second Slide: ThreeColumnSlide Component */}
-        <SlideWithInView>
-          <Suspense fallback={<LoadingSpinner />}>
-            <ThreeColumnSlide
+        {/* Second Slide */}
+
+        <SwiperSlide>
+          <InViewSlide>
+          <ThreeColumnSlide
               column2Title="Comprehensive Analysis and Key Insights"
               column2Text="In our 2024 ICE research, we took a deep dive into the events industry's ever-changing landscape. While sustainability is still a top priority for our team at ICE-HUB, we uncovered other fascinating insights, too."
               interactiveChart={<ThreeColumnGrid />} // Pass your interactive chart component here
             />
-          </Suspense>
-        </SlideWithInView>
+          </InViewSlide>
+        </SwiperSlide>
 
-        {/* Third Slide: ThreeColumnSlide Component */}
-        <SlideWithInView>
-          <Suspense fallback={<LoadingSpinner />}>
-            <ThreeColumnSlide
-              column2Title="ARE YOU AN EVENT STRATEGIST OR EVENT DELIVER?"
-              column2Text="Corporate event creators tend to identify themselves as fulfilling both roles—Event Strategists and Event Deliverers—with 46.6% selecting 'Both.' A significant portion (39.7%) identified solely as Event Strategists."
-              interactiveChart={<TreemapChart />} // Pass your interactive chart component here
-            />
-          </Suspense>
-        </SlideWithInView>
+        {/* Third Slide */}
 
-        {/* Fourth Slide: Team Structure and Size */}
-        <SlideWithInView>
-          <Suspense fallback={<LoadingSpinner />}>
-            <ThreeColumnSlide
-              column2Title="Team Structure and Size"
-              column2Text="In terms of team size, the majority of corporate event planners worked in small teams, with 43.1% reporting teams of 1-5 members, and 34.5% organising between 1 and 30 events per year."
-              interactiveChart={<Q2StackedBar />} // Pass your interactive chart component here
-            />
-          </Suspense>
-        </SlideWithInView>
+        <SwiperSlide>
+          <InViewSlide>
+          <ThreeColumnSlide
+                    column2Title="ARE YOU AN EVENT STRATEGIST OR EVENT DELIVER?"
+                    column2Text="Corporate event creators tend to identify themselves as fulfilling both roles—Event Strategists and Event Deliverers—with 46.6% selecting 'Both.' A significant portion (39.7%) identified solely as Event Strategists (focused on high-level planning and decision making), while a smaller group (13.8%) viewed themselves as primarily Event Deliverers (focused on logistics and execution). Therefore, most respondents see themselves involved in both strategic and operational aspects of event planning, with a slightly stronger leaning towards strategy."
+                    column3Title="EVENT STRATEGIST OR EVENT DELIVER"
+                    column3SubHead="Hover over and click on the chart to interact with the data."
+                    interactiveChart={<TreemapChart />} // Pass your interactive chart component here
+                />
+          </InViewSlide>
+        </SwiperSlide>
 
-        {/* Additional slides follow the same pattern */}
-        <SlideWithInView>
-          <Suspense fallback={<LoadingSpinner />}>
-            <ThreeColumnSlide
-              column2Title="How important is salary when choosing a job role?"
-              column2Text="The most important factor for respondents when choosing a job role is Salary/Compensation."
-              interactiveChart={<Q3RadarChart />} // Pass your interactive chart component here
-            />
-          </Suspense>
-        </SlideWithInView>
-
-        <SlideWithInView>
-          <Suspense fallback={<LoadingSpinner />}>
-            <ThreeColumnSlide
-              column2Title="Event Budgeting and Financial Management"
-              column2Text="Budget management remains a core part of the responsibilities for corporate event planners."
-              interactiveChart={<Q4Bar />} // Lazily loaded BarChart
-            />
-          </Suspense>
-        </SlideWithInView>
-
-        <SlideWithInView>
-          <Suspense fallback={<LoadingSpinner />}>
-            <ThreeColumnSlide
-              column2Title="Future Skills for Event Professionals"
-              column2Text="AI and technology proficiency were seen as the most important future skills for corporate event planners."
-              interactiveChart={<Q5Bubble />} // Lazily loaded BarChart
-            />
-          </Suspense>
-        </SlideWithInView>
-        <SlideWithInView>
-          <Suspense fallback={<LoadingSpinner />}>
-            <ThreeColumnSlide
-              column2Title="Future Skills for Event Professionals"
-              column2Text="AI and technology proficiency were seen as the most important future skills for corporate event planners."
-              interactiveChart={<Q5Bubble />} // Lazily loaded BarChart
-            />
-          </Suspense>
-        </SlideWithInView>
-        <SlideWithInView>
-          <Suspense fallback={<LoadingSpinner />}>
-            <ThreeColumnSlide
-              column2Title="Future Skills for Event Professionals"
-              column2Text="AI and technology proficiency were seen as the most important future skills for corporate event planners."
-              interactiveChart={<Q5Bubble />} // Lazily loaded BarChart
-            />
-          </Suspense>
-        </SlideWithInView>
-        <SlideWithInView>
-          <Suspense fallback={<LoadingSpinner />}>
-            <ThreeColumnSlide
-              column2Title="Future Skills for Event Professionals"
-              column2Text="AI and technology proficiency were seen as the most important future skills for corporate event planners."
-              interactiveChart={<Q5Bubble />} // Lazily loaded BarChart
-            />
-          </Suspense>
-        </SlideWithInView>
-        {/* <SlideWithInView>
-          <Suspense fallback={<LoadingSpinner />}>
-            <ThreeColumnSlide
-              column2Title="Future Skills for Event Professionals"
-              column2Text="AI and technology proficiency were seen as the most important future skills for corporate event planners."
-              interactiveChart={<Q5Bubble />} // Lazily loaded BarChart
-            />
-          </Suspense>
-        </SlideWithInView> */}
-
-        {/* Repeat for other slides */}
-      </Slider>
+        {/* Third Slide */}
+        <SwiperSlide>
+          <InViewSlide>
+          <ThreeColumnSlide
+                    column2Title="Team Structure and Size"
+                    column2Text="In terms of team size, the majority of corporate event planners worked in small teams, with 43.1% reporting teams of 1-5 members, and 34.5% organising between 1 and 30 events per year. This is an X increase of 2023 to 2024. External agencies (62.3%) and freelancers (37.7%) were commonly hired to manage workloads, particularly during periods of event overload. This reliance on external resources highlights the need for flexibility and the ability to scale up operations quickly in response to demand. 
+"
+                    column3Title="Team Structure and Size"
+                    column3SubHead="Hover over and click on the chart to interact with the data."
+                    interactiveChart={<Q2StackedBar />} // Pass your interactive chart component here
+                />
+          </InViewSlide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <InViewSlide>
+          <ThreeColumnSlide
+                    column2Title="How important is salary when choosing a job role?"
+                    column2Text="The most important factor for respondents when choosing a job role is Salary/Compensation, with an average rank of 2.35. This is closely followed by Company Culture and Work-Life Balance, which suggests that while financial considerations are crucial, workplace environment and personal well-being are also significant factors. Holiday Allowance is ranked as the least important, indicating that it may not be as immediate a concern compared to factors that impact day-to-day experiences."
+                    column3Title="Salary Importance"
+                    column3SubHead="Hover over and click on the chart to interact with the data."
+                    interactiveChart={<Q3RadarChart />} // Pass your interactive chart component here
+                />
+          </InViewSlide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <InViewSlide>
+          <ThreeColumnSlide
+                    column2Title="Event Budgeting and Financial Management"
+                    column2Text="Budget management remains a core part of the responsibilities for corporate event planners. Nearly half of the respondents (48.9%) stated that stakeholders were responsible for setting the event budget, while 46.7% indicated that the event team itself took on this responsibility."
+                    column3Title="Financial Management."
+                    column3SubHead="Hover over and click on the chart to interact with the data."
+                    interactiveChart={<Q4Bar />} // Lazily loaded BarChart
+                />
+          </InViewSlide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <InViewSlide>
+          <ThreeColumnSlide
+                    column2Title="Future Skills for Event Professionals"
+                    column2Text="As technology continues to transform the events industry, AI and technology proficiency were seen as the most important future skills for corporate event planners, with 91.1% of respondents emphasising the need for technical know-how. Data analysis skills were also highly valued (66.1%), as event planners seek to make data-driven decisions to optimise event performance. Stakeholder management (64.3%) and sustainability practices (57.1%) were other skills deemed essential for future success, underscoring the multifaceted nature of corporate event planning."
+                    column3Title="Most Important Future Skills"
+                    column3SubHead="Hover over and click on the chart to interact with the data."
+                    interactiveChart={<Q5Bubble />} // Lazily loaded BarChart
+                />
+          </InViewSlide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <InViewSlide>
+          <ThreeColumnSlide
+                    column2Title="Challenges Faced by Corporate Event Planners"
+                    column2Text="Budget constraints were reported as the most significant challenge for corporate event planners, with 47.7% of respondents ranking it as their top concern for the coming 12 months. Stakeholder management (22.7%) and technology integration (27.3%) were also highlighted as major hurdles. Additionally, 50% of planners expect their team sizes to remain the same over the next year, despite the increasing complexity of events. This highlights the pressure on event planners to deliver high-quality, innovative experiences with limited resources. Sustainability practices also presented a challenge, with 70.2% of respondents citing cost as the biggest barrier to incorporating sustainable initiatives into their events."
+                    column3Title="Challenges Faced by Event Planners"
+                    column3SubHead="Hover over and click on the chart to interact with the data."
+                    interactiveChart={<Q6RadarChart />} // Lazily loaded BarChart
+                />
+          </InViewSlide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <InViewSlide>
+          <ThreeColumnSlide
+                    column2Title="Technology Adoption"
+                    column2Text="The research highlights two dominant trends shaping the future of corporate event planning: the increased use of AI and technology (50.9%) and a greater focus on sustainability (36.4%). Many event planners already utilise AI to enhance various aspects of event management, with 41.4% of respondents reporting the use of AI in content generation, logistics, and data analysis. The rise of hybrid event formats was also noted, with 56.4% seeing it as an enduring format for the industry. As organisations become more conscious of their environmental impact, sustainability emerged as a priority, with 42.9% of respondents factoring it into their planning processes."
+                    interactiveChart={<Q7TechnologyAdoption />} // Lazily loaded BarChart
+                />
+          </InViewSlide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <InViewSlide>
+          <ThreeColumnSlide
+                    column2Title="ROI and Success Metrics"
+                    column2Text="The data suggests that event planners prioritise elements that directly impact the attendee experience, such as content creation, engagement, and budget management. While important, factors like technology integration and sustainability are considered less critical, indicating that planners might still view them as supporting, rather than central, components in delivering successful events. This highlights a potential opportunity for greater emphasis on sustainable practices and technological advancements in future event strategies."
+                    column3Title="Priorities when organising an event"
+                    column3SubHead="Hover over and click on the chart to interact with the data."
+                    interactiveChart={<Q8ROI />} // Lazily loaded BarChart
+                />
+          </InViewSlide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <InViewSlide>
+          <ThreeColumnSlide
+                    column2Title="ROI and Success Metrics"
+                    column2Text="When measuring the success of their events, the majority of corporate event planners prioritised attendee satisfaction, with 93.5% using this as a key metric. Post-event surveys were also widely used (76.1%), along with tracking the number of attendees (71.7%). Lead generation (58.7%) and content performance (52.2%) emerged as other significant indicators of success, reflecting the importance of events as not only networking opportunities but also as business development and marketing tools. Interestingly, 39.1% used net promoter scores (NPS) to gauge loyalty and satisfaction, while 37% monitored social media engagement, indicating a growing interest in digital touchpoints and audience sentiment post-event."
+                    column3Title="Metrics used to measure the success of your events"
+                    column3SubHead="Hover over and click on the chart to interact with the data."
+                    interactiveChart={<Q9Metrics />} // Lazily loaded BarChart
+                />
+          </InViewSlide>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
 
-// Function to handle the slide visibility using the intersection observer
-function SlideWithInView({ children }) {
-  const { ref, inView } = useInView({
-    triggerOnce: true, // Load only once when in view
-    threshold: 0.1, // Adjust threshold for when to consider in view
-  });
-
-  return (
-    <div ref={ref}>
-      {inView ? children : <LoadingSpinner />} {/* Render the component or fallback based on visibility */}
-    </div>
-  );
+// In-view hook for optimising performance
+function InViewSlide({ children }) {
+  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.1 });
+  return <div ref={ref}>{inView && children}</div>;
 }
